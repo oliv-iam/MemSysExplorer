fb804a146d472061a19ff3e35a7d4aea3c06a62a
Olivia <oliviamfann@gmail.com>
2025-09-23 13:08:56 -0400

    basic run.py sketch

---
+++ run.py
@@ -0,0 +1,9 @@
+# <imports / functions>
+
+if __name__ == '__main__':
+    
+    # get apps benchmark, tech config as arguments
+    # pull read/write info from apps benchmark
+    # run tech side -> read/write latency & power
+    # calculations (total time / power)
+    # dump apps output, tech output, totals in csv
